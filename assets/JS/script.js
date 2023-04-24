e6417ce06279a74761c780cf6e9814b05c7ba5d9
andreaconv <andrea2convertini@gmail.com>
2023-04-24 15:39:52 +0200

    visualizzazione mail generata

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,14 +5,19 @@ createApp({
   data(){
 
     return{
-      
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      mailGenerata: null,
     }
 
   },
 
   methods:{ //FUNZIONI
     getApi(){
-      console.log("vue")
+      axios.get(this.apiUrl)
+      .then(result => {
+        console.log(result.data.response)
+        this.mailGenerata = result.data.response;
+      })
     }
   },
 
